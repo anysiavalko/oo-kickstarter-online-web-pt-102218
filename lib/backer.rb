769d71eb9e678103844b2d4e81cd8d3954b11daa
Anysia V <anysiavalko@gmail.com>
2019-01-06 21:10:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,20 @@
 class Backer
   attr_accessor :name
   
-  def backed_projects
-    @backed_projects = [ ]
+  @@backed_projects = [ ]
+  
+  def self.backed_projects
+    @@backed_projects
   end
+  
   
   def initialize(name)
     @name = name 
   end
   
   def back_project(project)
-    @backed_projects << project
+    @@backed_projects << project
+    project.backer = self
   end 
     
   
